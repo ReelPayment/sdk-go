0eeb5a9f4dba37d9f2054b3fe582163e8370326e
ReelPay <139612142+ReelPayment@users.noreply.github.com>
2023-07-18 15:58:59 +0800

    Add files via upload

---
+++ application/transactions.go
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the ReelPay SDK-GO package.
+ *
+ * (c) ReelPay <support@reelpay.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+package application
+
+import "reelpay/httpRequest"
+
+type Transactions struct {
+	AppID  string
+	AppKey string
+}
+
+func NewTransactions(appID, appKey string) *Transactions {
+	return &Transactions{AppID: appID, AppKey: appKey}
+}
+
+func (t *Transactions) Pay(req *PayRequest) (*TransactionResponse, error) {
+	response := new(TransactionResponse)
+	err := httpRequest.Request("/v1/transactions/pay", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+
+func (t *Transactions) EntrustPay(req *EntrustRequest) (*EntrustResponse, error) {
+	response := new(EntrustResponse)
+	err := httpRequest.Request("/v1/transactions/entrust", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+
+func (t *Transactions) Amount(req *AmountRequest) (*AmountResponse, error) {
+	response := new(AmountResponse)
+	err := httpRequest.Request("/v1/transactions/amount", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+
+func (t *Transactions) Transaction(req *TransactionRequest) (*TransactionResponse, error) {
+	response := new(TransactionResponse)
+	err := httpRequest.Request("/v1/transactions", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+
+func (t *Transactions) Close(req *TransactionRequest) (*Response, error) {
+	response := new(Response)
+	err := httpRequest.Request("/v1/transactions/close", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+func (t *Transactions) Refund(req *RefundRequest) (*Response, error) {
+	response := new(Response)
+	err := httpRequest.Request("/v1/transactions/refund", t.AppID, t.AppKey, req, response)
+	return response, err
+}
+
+func (t *Transactions) Currency(req *CurrencyRequest) (*CurrencyResponse, error) {
+	response := new(CurrencyResponse)
+	err := httpRequest.Request("/v1/transactions/currency", t.AppID, t.AppKey, req, response)
+	return response, err
+}
---
+++ application/transactions_test.go
@@ -0,0 +1,64 @@
+package application
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTransactions_Currency(t *testing.T) {
+	res, err := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").Currency(&CurrencyRequest{})
+	fmt.Println(res, err)
+}
+
+func TestTransactions_EntrustPay(t *testing.T) {
+	res, _ := NewTransactions("el9q0mzllpjhducy", "VqdqyUvFt5vQcmJm78dbdLMeEszdzT8I").EntrustPay(&EntrustRequest{
+		OutTradeNo: "jSWfrolOTdsadcYuUwkJbdw9IJUBeV",
+		Symbol:     "USD",
+		Amount:     "0.01",
+		Name:       "Product name",
+		Image:      "https://reelpay.com/product.jpg",
+	})
+	fmt.Println(res)
+}
+
+func TestTransactions_Amount(t *testing.T) {
+	res, _ := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").Amount(&AmountRequest{
+		CurrencyID: "7bifjodnuQvsyU7umAiveISacVbBQT7A",
+		FiatName:   "USD",
+		FiatAmount: "0.000000001",
+	})
+	fmt.Println(res)
+}
+
+func TestTransactions_Pay(t *testing.T) {
+	res, err := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").
+		Pay(&PayRequest{
+			OutTradeNo: "QYXwrw25PzFZPYw83XWC5ManQWH3SZaW",
+			CurrencyID: "BeaMChTcRrC0UkKdSvbLLl9he06RuE01",
+			FiatName:   "Usd",
+			FiatAmount: "1",
+		})
+	fmt.Println(res, err)
+}
+
+func TestTransactions_Transaction(t *testing.T) {
+	res, err := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").Transaction(&TransactionRequest{
+		TradeNo: "fd720940e69e584c9820230425150922",
+	})
+	fmt.Println(res, err)
+}
+
+func TestTransactions_Close(t *testing.T) {
+	res, _ := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").Close(&TransactionRequest{
+		TradeNo: "5ad7aa46bc7b3daf1720230424164751",
+	})
+	fmt.Println(res)
+}
+
+func TestTransactions_Refund(t *testing.T) {
+	res, err := NewTransactions("sffkuubyosc9i63w", "8b4jSWfrolOTYuUwkJbdw9IJUBeVWK3O").Refund(&RefundRequest{
+		TradeNo: "uz54NEW9TVhLSIDHUSYB4aEFupKoE7I8",
+		Fuel:    1,
+	})
+	fmt.Println(res, err)
+}
